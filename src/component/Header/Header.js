ec468171ced53bd784dd0260008078116dd04b68
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2019-02-14 21:17:24 +0530

    initial baseline

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="header-title">
+            <h3>To do List</h3>
+        </div>
+    )
+}
+
+export default Header;
